eadf730d751ebdac0681f44d63c7400da3dd0185
mjhelf <mjh438@cornell.edu>
2017-06-22 17:07:22 -0400

    Initial commit

---
+++ App/app.R
@@ -0,0 +1,7 @@
+#library(shiny)
+source("F:/Users$/mjh438/R scripts/EIC_reader_v8-functions_p9.R")
+
+options(shiny.port=1899)
+
+#shinyApp(ui = ui, server = server, enableBookmarking = "disable")
+runApp("F:/Users$/mjh438/R scripts/Mosaic/Version 002/")
