bfb181d290bbf9c504590bf7141c5df6912eaf69
mjhelf <mjh438@cornell.edu>
2017-10-06 12:26:19 -0400

    Updated bookmarking to include rt correction data

--- R/runMosaic.R
+++ R/runMosaic.R
@@ -14,7 +14,7 @@ runMosaic <- function(devel = F, server = T, ...) {
   
   appDir <- system.file("MosaicApp", "main", package = "Mosaic")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `Mosaic`.", call. = FALSE)
+    stop("Could not find Mosaic directory. Try re-installing `Mosaic`.", call. = FALSE)
   }
   
   shiny::runApp(appDir, ...)
