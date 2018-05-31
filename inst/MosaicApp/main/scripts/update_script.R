44dce48ad79c2b95d77edbe4a5774ba173780d63
mjhelf <mjh438@cornell.edu>
2018-05-31 18:53:05 -0400

    Updater

--- inst/MosaicApp/main/scripts/update_script.R
+++ inst/MosaicApp/main/scripts/update_script.R
@@ -1,14 +1,17 @@
 
 tryCatch({
   print("Please wait while MOSAIC is being updated")
-  Sys.sleep(10)
+  Sys.sleep(5)
   message("Mosaic version before update: ", packageVersion("Mosaic")[1])
   devtools::install_github("mjhelf/Mosaic", ref = commandArgs(trailingOnly=TRUE)[1], quiet = F)
   message("Mosaic version after update: ", packageVersion("Mosaic")[1])
-  print("update complete")
-  Sys.sleep(10)
+  print("update complete!")
+  print("You can close this window now.")
+  Sys.sleep(Inf)
 },
 error = function(e){
-  print("Installation failed")
+  print("Installation failed. Try to install from the R console using the command:")
+  print(paste0('devtools::install_github("mjhelf/Mosaic", ref ="',commandArgs(trailingOnly=TRUE)[1],'")'))
+  print("You can close this window now.")
+  Sys.sleep(Inf)
 })
-
