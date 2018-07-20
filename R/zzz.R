193bec8f387470d9111a8fb3c3e847821db4e1ee
Max Helf <mjh438@cornell.edu>
2018-07-20 14:42:12 -0400

    fixes to shinytests and getgauss() test

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
     paste("\nWelcome to MOSAiC version",
-          packageVersion("Mosaic"),
+          utils::packageVersion("Mosaic"),
           "\nVisit our websites: http://mosaic.bti.cornell.edu and https://github.com/mjhelf/Mosaic\n"))
 }
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(Mosaic)
 library(shinytest)
-
+library(xcms) #getgauss() does not work otherwise -- needs fix!
 
 #test_dir("./tests/testthat/")
 #test_dir("./tests/testthat/shinytest")
