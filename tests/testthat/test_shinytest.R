a37d9616cb7fb4c2075cf23341d067a61f17fe32
mjhelf <mjh438@cornell.edu>
2018-07-21 19:06:34 -0400

    clean up

--- tests/testthat/test_shinytest.R
+++ tests/testthat/test_shinytest.R
@@ -2,12 +2,12 @@
 context("Shiny tests")
 
 test_that("Application works", {
-  expect_pass(testApp("MinimalMosaic", "mytest.R", compareImages = FALSE))
-  expect_pass(testApp("MinimalMosaicExampleData", "mytest.R", compareImages = FALSE))
+  expect_pass(testApp("MinimalMosaic", "mytest.R", compareImages = FALSE, quiet = T))
+  expect_pass(testApp("MinimalMosaicExampleData", "mytest.R", compareImages = FALSE, quiet = T))
 })
 
 #' NOTES:
-#' in "MinimalMosaicExampleData, 1714942.5 is rounded down to 1714942 in Linux, but rounded up to 1714943 in Windows.
+#' in "MinimalMosaicExampleData", 1714942.5 is rounded down to 1714942 in Linux, but rounded up to 1714943 in Windows.
 #' Expected example output is the linux value
 #' 
 #' 
