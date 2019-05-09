e69da46dcfd9dc4e9a3b885d3cecead18d3b01a4
mjhelf <mjh438@cornell.edu>
2019-05-09 17:18:30 -0400

    shinytest working

--- tests/testthat/FullMseek/tests/mytest.R
+++ tests/testthat/FullMseek/tests/mytest.R
@@ -1,7 +1,7 @@
 app <- ShinyDriver$new("../", loadTimeout = 120000)
-Sys.sleep(5)
+Sys.sleep(15)
 app$snapshotInit("mytest")
 
 app$setInputs(`Mseek-MseekSB` = "exploredata")
-Sys.sleep(10)
+Sys.sleep(15)
 app$snapshot()
--- tests/testthat/test_app.R
+++ tests/testthat/test_app.R
@@ -9,6 +9,11 @@ context("Run Mseek")
 # writeLines(inp2,"./tests/testthat/FullMseek/testresult.json")
 
 
+#all.equal(inp2, readLines("./tests/testthat/FullMseek/expected_testresult.json"))
+
+#expect_mapequal(tres$input, jsonlite::fromJSON("./tests/testthat/FullMseek/expected_testresult.json")$input)
+
+#expect_setequal(inp2, readLines("./tests/testthat/FullMseek/expected_testresult.json"))
 #testApp("./tests/testthat/FullMseek", "mytest.R", compareImages = FALSE, quiet = F)
 
 # test_that("Mseek app starts up", {
@@ -23,7 +28,21 @@ test_that("Mseek app starts up", {
     
     inp <- readLines("FullMseek/tests/mytest-expected/001.json")
     inp2 <- gsub(.MseekOptions$recentProjects[1], "<RECENT_PROJECT>", inp)
-   expect_equal(readLines("FullMseek/testresult.json"),inp2)
+    writeLines(inp2,"FullMseek/tests/mytest-expected/001.json")
+    
+    expect_setequal(readLines("FullMseek/expected_testresult.json"),inp2)
+    
+    
+})
+
+test_that("Mseek app starts up (stricter check on output)", {
+  
+  expected <- jsonlite::fromJSON("FullMseek/expected_testresult.json")
+  thisrun <- jsonlite::fromJSON("FullMseek/tests/mytest-expected/001.json")
+  
+  expect_mapequal(expected$input, thisrun$input)
+  expect_mapequal(expected$output, thisrun$output)
+  
 })
 
 #' NOTES:
