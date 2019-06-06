a6c92f4b927af0c4856d8180b2829da4f666b89d
mjhelf <mjh438@cornell.edu>
2019-06-06 16:12:57 -0400

    documentation

--- R/Functions_deprecated.R
+++ R/Functions_deprecated.R
@@ -1,3 +1,37 @@
+#' TableUpdateChunk
+#' 
+#' @section NOTE:
+#' This function is deprecated. Use \code{\link{updateFT}} instead!
+#' 
+#' Code chunk to update the active Feature Table before triggering a
+#'  rerendering of the rhandsontable object.
+#'  
+#' Chunks are evaluated in their parent environment, and therefore require
+#'  all objects they work on to be present under their correct names.
+#' 
+#' @details 
+#' \subsection{requires}{
+#' 
+#' values$featureTables$Maintable - pointing to the MainTableObject
+#' values$featureTables
+#' }
+#' 
+TableUpdateChunk <- function(){
+    eval.parent(quote({
+        if(!is.null(values$featureTables)
+           && !is.null(values$featureTables$Maintable) 
+           && values$featureTables$Maintable$hasUpdates){
+            if((!is.null(values$featureTables$tables[[values$featureTables$active]]$editable) 
+                && values$featureTables$tables[[values$featureTables$active]]$editable)
+               || is.null(values$featureTables$Maintable$liveView$comments) ){
+                values$featureTables$tables[[values$featureTables$active]]$df[row.names(values$featureTables$Maintable$liveView),colnames(values$featureTables$Maintable$liveView)] <- values$featureTables$Maintable$liveView
+            }else{
+                values$featureTables$tables[[values$featureTables$active]]$df[row.names(values$featureTables$Maintable$liveView),"comments"] <- values$featureTables$Maintable$liveView$comments
+            }
+        }
+        
+    }))
+}
 
 #' getAllScans
 #'
